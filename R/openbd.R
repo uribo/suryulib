fc3841c98a61cef6e899fad63af77606218a8e46
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2022-07-11 14:38:01 +0900

    [add]openBD API

---
+++ R/openbd.R
@@ -0,0 +1,20 @@
+#' Request to openBD API
+#'
+#' @param isbn isbn code
+#' @examples
+#' \dontrun{
+#' get_openbd("9784478108536")
+#' }
+#' @export
+get_openbd <- function(isbn) {
+  if (length(isbn) == 0) {
+    rlang::abort("ISBNを入力してくだい")
+  } else {
+    x <-
+      httr2::request("https://api.openbd.jp/v1/get") %>%
+      httr2::req_url_query(isbn = paste0(isbn, collapse = ","))
+    x %>%
+      httr2::req_perform() %>%
+      httr2::resp_body_json()
+  }
+}
